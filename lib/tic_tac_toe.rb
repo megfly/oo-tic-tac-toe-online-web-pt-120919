eed5d1c5596d5d8b30643e0e27002e4c3352c87d
Meghan Flynn <mflynn333@gmail.com>
2020-01-23 23:17:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,4 +100,13 @@ end
     won? || full? || draw?
   end 
   
+  def winner 
+    WIN_COMBINATIONS.detect do |win_combo|
+      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+        return "X"
+      else if (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+        return "O"
+  end 
+end 
+  
 end 
